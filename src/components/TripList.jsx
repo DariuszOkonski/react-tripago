cb4969cb002db2ec7153ac1f48dfb62ae08b8452
Dariusz Okonski <darek200180@gmail.com>
2021-11-25 02:35:23 +0000

    create user hook useFetch

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import './TripList.css';
 
 export default function TripList() {    
@@ -6,18 +6,20 @@ export default function TripList() {
     const [trips, setTrips] = useState([]);
     const [url, setUrl] = useState('http://localhost:3000/trips')
 
-    const fetchTrips = async() => {
-        const response = await fetch(url);
-        const json = await response.json();
-        setTrips(json);
-    }
-
+    const fetchTrips = useCallback(
+        async () => {
+            const response = await fetch(url);
+            const json = await response.json();
+            setTrips(json);
+        },
+        [url]
+    )
 
     useEffect(() => {
         
         fetchTrips();
 
-    }, [url]);
+    }, [fetchTrips]);
 
     console.log(url)
     
