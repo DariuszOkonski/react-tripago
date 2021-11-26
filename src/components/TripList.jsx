7d39a805592c996903286db0fb8c8d478754a518
Dariusz Okonski <darek200180@gmail.com>
2021-11-26 02:12:04 +0000

    add error handling

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -5,7 +5,7 @@ import './TripList.css';
 export default function TripList() {    
     
     const [url, setUrl] = useState('http://localhost:3000/trips')
-    const { data: trips, isPending } = useFetch(url);
+    const { data: trips, isPending, error } = useFetch(url);
 
 
     console.log(url)
@@ -13,12 +13,8 @@ export default function TripList() {
     return (
         <div className="trip-list">
             <h2>Trip list</h2>
-            {
-                isPending &&
-                <div>
-                    <h1>Loading trips...</h1>                    
-                </div>
-            }
+            { isPending && <div>Loading trips...</div> }
+            { error && <div>{error}</div> }
             <ul>
                 {
                     trips && trips.map(trip => {
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,22 +3,36 @@ import { useEffect, useState } from "react";
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(false);
-
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setIsPending(true);
         const fetchData = async () => {
 
-            const res = await fetch(url);
-            const json = await res.json();
+            try {
+                const res = await fetch(url);
+                if(!res.ok) {
+                    throw Error(res.statusText)                    
+                }
+                
+                const json = await res.json();
+                
+                setIsPending(false);
+                setData(json);
+                setError(null);
+
+                // catch - works only when no internet connection
+            } catch (err) {
+                setIsPending(false);
+                setError('Could not fetch the data');
+                console.log(err.message);
+            }
             
-            setIsPending(false);
-            setData(json);
         }
         fetchData();
 
     }, [url]);
     
 
-    return { data, isPending }
+    return { data, isPending, error }
 }
